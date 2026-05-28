bff296d2a15bdd05e5502a2470a64fef8063cfd9
Alexis Castellanos <castellanos.alexis@hotmail.com>
2026-05-27 21:52:13 -0600

    feat 9: useRef (#9)

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 // import { HooksApp } from "./HooksApp";
 
 import "./index.css";
-import { PokemonPage } from "./03-examples/PokemonPage";
+// import { PokemonPage } from "./03-examples/PokemonPage";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 // import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
 // import { TrafficLight } from "./01-useState/TrafficLight";
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
 		{/* <TrafficLight /> */}
 		{/* <TrafficLightWithEffect /> */}
 		{/* <TrafficLightWithHook /> */}
-		<PokemonPage />
+		{/* <PokemonPage /> */}
+		<FocusScreen />
 	</StrictMode>,
 );
