58ba80ca81b12ba2d8da0c115dc522f4200956d2
Alexis Castellanos <castellanos.alexis@hotmail.com>
2026-05-29 19:29:09 -0600

    feat 10: setup shadcn/ui

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 // import { PokemonPage } from "./03-examples/PokemonPage";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+import { TasksApp } from "./05-useReducer/TasksApp";
 // import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
 // import { TrafficLight } from "./01-useState/TrafficLight";
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
 		{/* <TrafficLightWithEffect /> */}
 		{/* <TrafficLightWithHook /> */}
 		{/* <PokemonPage /> */}
-		<FocusScreen />
+		{/* <FocusScreen /> */}
+		<TasksApp />
 	</StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
@@ -11,4 +12,9 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
